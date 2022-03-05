b18a53949bd8bddb395f464f908ef970482975e0
SamGastesi <100959574+SamGastesi@users.noreply.github.com>
2022-03-04 22:12:16 -0500

    Update suma2.py

--- Operaciones Básicas/Suma/suma2.py
+++ Operaciones Básicas/Suma/suma2.py
@@ -1,1 +1,6 @@
+# Tenemos dos variables a las cuales les asignamos diferentes valores, abajo sumamos estas dos y en la ultima linea nos arroja el resultado.
 
+num1 = 30
+num2 = 70
+sum1 = num1 + num2
+print(sum1)
