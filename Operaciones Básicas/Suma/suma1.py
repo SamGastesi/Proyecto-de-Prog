f7f522e00e3abdf7e6ab916aa7153ecf0d15faff
SamGastesi <100959574+SamGastesi@users.noreply.github.com>
2022-03-04 22:00:50 -0500

    Update suma1.py

--- Operaciones Básicas/Suma/suma1.py
+++ Operaciones Básicas/Suma/suma1.py
@@ -1,4 +1,4 @@
-# A la izquierda del operador de asignación nos encontramos con una variable y a la derecha la peracion de dos números. 
+# A la izquierda del operador de asignación nos encontramos con una variable y a la derecha la operacion de dos números. 
 s = 23 + 42
 
 print(s)
