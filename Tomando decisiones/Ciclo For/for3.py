39c4f6e3d9f34124cdaaed5461c89e0bf5e1a93e
SamGastesi <100959574+SamGastesi@users.noreply.github.com>
2022-03-06 00:34:35 -0500

    Update for3.py

--- Tomando decisiones/Ciclo For/for3.py
+++ Tomando decisiones/Ciclo For/for3.py
@@ -1,1 +1,16 @@
+# Ejercicio de for
 
+positivos = 0; negativos = 0; neutros = 0
+
+for i in range(6):
+    num = int(input('Ingrese un número: '))
+    if num > 0:
+        positivos += 1
+    elif num < 0:
+        negativos += 1
+    else:
+        neutros += 1
+
+print('Total de positivos:', positivos)
+print('Total de negativos:', negativos)
+print('Total de neutros:', neutros)
