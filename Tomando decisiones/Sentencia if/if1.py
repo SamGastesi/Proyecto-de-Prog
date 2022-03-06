aa6032009ed88dbb77428514d107902e8bbe1c48
SamGastesi <100959574+SamGastesi@users.noreply.github.com>
2022-03-06 01:25:01 -0500

    Update if1.py

--- Tomando decisiones/Sentencia if/if1.py
+++ Tomando decisiones/Sentencia if/if1.py
@@ -1,1 +1,18 @@
+# Ejercicio de if
 
+a = (70 - 95)
+b = 80
+c = 90
+
+if a >= 70:
+    print("continuemos")
+    if b == 70:
+        print("incorrecto")
+    elif b == 80:
+        print("continuemos")
+        if c > 90:
+            print("incorrecto")
+        elif c > 70:
+            print("Las condiciones son correctas")
+elif a < 70:
+    print("Las condiciones son incorrectas")
