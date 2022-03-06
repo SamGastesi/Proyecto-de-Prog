c0b65d5436f8336c39f9e6fb6123ae755420b33e
SamGastesi <100959574+SamGastesi@users.noreply.github.com>
2022-03-05 23:50:12 -0500

    Update tuple2.py

--- Tipos de datos/Tuple/tuple2.py
+++ Tipos de datos/Tuple/tuple2.py
@@ -1,1 +1,17 @@
+# Ejercicio de tuple
+
+ejemplo = input("Ingrese una muestra de valores númericos separados por comas: ")
+ejemplo = ejemplo.split(',')
+n = len(ejemplo)
+for i in range(n):
+    ejemplo[i] = int(ejemplo[i])
+ejemplo = tuple(ejemplo)
+sum = 0
+sumsq = 0
+for i in ejemplo:
+    sum += i
+    sumsq += i**2
+mean = sum/n
+stdev = (sumsq/n-mean**2)**(1/2)
+print('La media es', mean, ', y la desviación típica es', stdev)
 
