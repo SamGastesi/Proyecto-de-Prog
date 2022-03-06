3f25afaf0f323f3064aada1b86bae106476ca0a0
SamGastesi <100959574+SamGastesi@users.noreply.github.com>
2022-03-06 00:32:50 -0500

    Update for2.py

--- Tomando decisiones/Ciclo For/for2.py
+++ Tomando decisiones/Ciclo For/for2.py
@@ -1,1 +1,9 @@
+# Ejercicio de for
 
+n = 6
+for i in range(n):
+    num = int(input('Ingrese un número: '))
+    if num > 0:
+        print(num)
+    else:
+        print('Ingrese números positivos')
