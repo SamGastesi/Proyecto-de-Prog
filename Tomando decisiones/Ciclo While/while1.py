d25b2de45edad659af0bf8a05b4499ca344e7116
SamGastesi <100959574+SamGastesi@users.noreply.github.com>
2022-03-06 00:36:34 -0500

    Update while1.py

--- Tomando decisiones/Ciclo While/while1.py
+++ Tomando decisiones/Ciclo While/while1.py
@@ -1,1 +1,10 @@
+# Ejercicio de while
 
+d = 0
+n  = int(input("Número (0 para terminar): "))
+while n != 0:
+    if n > 0:
+        d += 1
+    n = int(input("Número (0 para terminar): ")) 
+    
+print("Cantidad de positivos:", d)
