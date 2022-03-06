1de3cde7f5630741b63314eba7983e59d669bed6
SamGastesi <100959574+SamGastesi@users.noreply.github.com>
2022-03-05 22:52:24 -0500

    Update string1.py

--- Tipos de datos/String/string1.py
+++ Tipos de datos/String/string1.py
@@ -1,1 +1,8 @@
+# Ejercicio de string
 
+dog = input("Ingrese una cadena: ")
+while True:
+    dog = input("Ingrese un carácter: ")
+    if len(dog) == 1: break
+
+print("En la cadena",dog,"aparecen",dog.count(dog),"veces el carácter",dog)
