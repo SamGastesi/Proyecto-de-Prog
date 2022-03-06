b3fdf74302b9123b61c554b6c7b83a9bac809eee
SamGastesi <100959574+SamGastesi@users.noreply.github.com>
2022-03-06 01:31:20 -0500

    Update if3.py

--- Tomando decisiones/Sentencia if/if3.py
+++ Tomando decisiones/Sentencia if/if3.py
@@ -1,1 +1,11 @@
+# Ejercicio de if
 
+w = 75
+
+if w %  2 == 0:
+    if w > 10:
+        print("El número es par y es mayor que 10")
+    else:
+        print("El número es par, pero no mayor 10")
+else:
+    print ("El número no es par.")
