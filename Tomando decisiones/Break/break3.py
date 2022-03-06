d4bea64e0a25f595296176b5d1f165edf91040a8
SamGastesi <100959574+SamGastesi@users.noreply.github.com>
2022-03-06 00:28:35 -0500

    Update break3.py

--- Tomando decisiones/Break/break3.py
+++ Tomando decisiones/Break/break3.py
@@ -1,1 +1,8 @@
+# Ejercicio de break
 
+for k in range(0, 9):
+    for p in range(0, 9):
+        break
+        # Nunca debemos hacer más de una iteracción
+        # El break no afecta a este for
+    print(k, p)
